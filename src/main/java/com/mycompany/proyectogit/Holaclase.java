210cc9ad886aa95403afe32d13faa0a234986c13
Sebas <sebastian.gavvel@educa.jcyl.es>
2024-04-25 17:04:02 +0200

    tercer commit del proyecto en netbeans

--- src/main/java/com/mycompany/proyectogit/Holaclase.java
+++ src/main/java/com/mycompany/proyectogit/Holaclase.java
@@ -1,17 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.proyectogit;
 
 /**
  *
  * @author admin
  */
-public class ProyectoGit {
-
-    public static void main(String[] args) {
-        System.out.println("Hello W orld! ");
-        System.out.println("Hello W orld!");
-    }
+public class Holaclase {
+    
 }
